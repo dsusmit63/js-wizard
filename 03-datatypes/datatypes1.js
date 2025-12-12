5077082ed283b40c69aa6e0dac79f70dc81c867e
dsusmit63 <dsusmit63@gmail.com>
2025-12-12 23:27:11 +0530

    updated datatypes1.js

--- 03-datatypes/datatypes1.js
+++ 03-datatypes/datatypes1.js
@@ -6,16 +6,16 @@ let x = 12232238233232939923n;
 let y;
 let z = null;
 
-console.log(typeof fullName);
-console.log(typeof age);
-console.log(typeof isLoggedIn);
-console.log(typeof x);
-console.log(typeof y);
-console.log(typeof z);
-console.log(typeof { firstName: "John", lastName: "Doe", age: 21 });
-console.log(typeof [10, 20, 30, 40, 50]);
+console.log(typeof fullName); //string
+console.log(typeof age); //number
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof x); //bigint
+console.log(typeof y); //undefined
+console.log(typeof z); //object
+console.log(typeof { firstName: "John", lastName: "Doe", age: 21 }); //object
+console.log(typeof [10, 20, 30, 40, 50]); //object
 console.log(
   typeof function hello() {
     console.log("Hello World");
   }
-);
+); //function
