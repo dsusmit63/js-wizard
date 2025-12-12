0e7eadc35a978a8c9cdca115663bd8c95fad8e46
dsusmit63 <dsusmit63@gmail.com>
2025-12-12 20:59:22 +0530

    added variable1.js

---
+++ 02-variable/variable1.js
@@ -0,0 +1,11 @@
+let userId; //undefined
+console.log(userId);
+userId = "239271";
+let userName = "John Doe";
+const aadharNumber = "2332-2837-8448";
+var userEmail = "johndoe21@gmail.com";
+userPhoneNumber = "8293631021"; //Not recommended
+
+// aadharNumber = "2392-8292-0292"; //Error:Assignment to constant variable
+
+console.table([userId, userName, aadharNumber, userEmail, userPhoneNumber]);
