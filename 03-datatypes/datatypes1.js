3a8cc39da94dc629b41c2206fd443654ca767d40
dsusmit63 <dsusmit63@gmail.com>
2025-12-12 23:24:56 +0530

    added datatypes1.js

---
+++ 03-datatypes/datatypes1.js
@@ -0,0 +1,21 @@
+let fullName = "John Doe";
+let designation = ""; //Empty String
+let age = 21;
+let isLoggedIn = true;
+let x = 12232238233232939923n;
+let y;
+let z = null;
+
+console.log(typeof fullName);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof { firstName: "John", lastName: "Doe", age: 21 });
+console.log(typeof [10, 20, 30, 40, 50]);
+console.log(
+  typeof function hello() {
+    console.log("Hello World");
+  }
+);
