1df820960b0f1c2a6df286506d7586d947e91226
dsusmit63 <dsusmit63@gmail.com>
2025-12-13 07:31:37 +0530

    added datatypes3.js

---
+++ 03-datatypes/datatypes3.js
@@ -0,0 +1,45 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue); //-3
+
+//Basic Mathematical Operations
+console.log(2 + 2); //4
+console.log(2 - 2); //0
+console.log(2 * 2); //4
+console.log(2 ** 3); //8
+console.log(2 / 2); //1
+console.log(2 % 2); //0
+
+//String Concatination
+let str1 = "Hello";
+let str2 = " World";
+let str3 = str1 + str2;
+console.log(str3); //Hello World
+
+//Operation between different types
+console.log("1" + 1); //11
+console.log(1 + "1"); //11
+console.log("1" + 1 + 1); //111
+console.log(1 + 1 + "1"); //21
+
+console.log(true); //true
+console.log(false); //false
+
+console.log(+true); //1
+console.log(+false); //0
+console.log(-true); //-1
+
+// console.log(true+);//Error
+
+console.log(+""); //0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 * 2;
+console.log(num1 + num2 + num3); //12
+
+//Increment Decrement Operator
+let num = 10;
+console.log(++num); //11
+console.log(num++); //11
+console.log(num--); //12
+console.log(--num); //10
