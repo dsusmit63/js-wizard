8c4b04c3b3ea49632d46107b5934274d835158f3
dsusmit63 <dsusmit63@gmail.com>
2025-12-13 09:01:41 +0530

    added operators1.js

---
+++ 04-operators/operators1.js
@@ -0,0 +1,23 @@
+//Arithmetic Operators - +,-,*,**,/,%
+let num1 = 20;
+let num2 = 10;
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let exp = num1 ** num2;
+let div = num1 / num2;
+let mod = num1 % num2;
+console.log(sum); //30
+console.log(sub); //10
+console.log(mul); //200
+console.log(exp); //10240000000000
+console.log(div); //2
+console.log(mod); //0
+
+//Increment & Decrement Operator
+let x = 10;
+x++;
+console.log(++x); //12
+console.log(--x); //11
+console.log(x--); //11
+console.log(x * 2); //20
